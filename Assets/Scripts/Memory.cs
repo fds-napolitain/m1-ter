bb426eec9f4bcdfa9d47aabaf40058e8a5aa706f
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2021-02-24 12:24:46 +0100

    J'ai fait des trucs

--- Assets/Scripts/Memory.cs
+++ Assets/Scripts/Memory.cs
@@ -5,31 +5,64 @@ using UnityEngine;
 public class Memory : MonoBehaviour
 {
 
-    GameObject carte;
-    GameObject carte_dos;
+    Sprite carte;
+    Sprite carte_dos;
 
     public int cp = 0;
 
     public string carte_prec;
 
-    public int erreur; //maximum 2
+    public string carte_Name;
 
+    public int erreur = 2; //maximum 2
 
+    private bool isUpsideDown = false;
 
-
-
-
-
-
-
-
+    private SpriteRenderer spriteRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        carte_dos = spriteRenderer.sprite;
+
+    }
+
+    public string cardName
+    {
+        get
+        {
+            return card_Name;
+        }
+        set
+        {
+            card_Name = value;
+        }
+    }
 
 
+    private void OnMouseDown() //This function is called each time player clicks on GameObject
+    {
+        if (!isUpsideDown && cp < erreur)
+        {
+            ChangeSide();
+            cp ++;
+        }
+    }
+
+    void ChangeSide()
+    {
+        if (!isUpsideDown)
+        {
+            spriteRenderer.sprite = carte;
+            isUpsideDown = true;
+        }
+        else
+        {
+            spriteRenderer.sprite =carte_dos;
+            isUpsideDown = false;
+        }
     }
 
 
