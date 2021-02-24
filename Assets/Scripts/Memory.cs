ae117324d5f8da80106376066483fce318e211c0
Mceresole <45624502+Mceresole@users.noreply.github.com>
2021-02-24 12:07:49 +0100

    début script (echec)

---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+
+    GameObject carte;
+    GameObject carte_dos;
+
+    public int cp = 0;
+
+    public string carte_prec;
+
+    public int erreur; //maximum 2
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
