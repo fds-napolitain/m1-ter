a8a454e55ae029c4fdd9aeb4ad5be11d09515699
Pauline C <47113664+NeonVhenan@users.noreply.github.com>
2021-02-25 12:34:19 +0100

    GameStateManager updated (test possible)

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -6,15 +6,92 @@ using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
+
+    public bool isFinished;
+
+    public string Intro;
+    public string SampleScene;
+    public string Ending;
+
+    public string Memory;
+    public string Simon;
+
+    private string currentScene;
+
+    public float lastUpdate;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        isFinished = false;
+        DontDestroyOnLoad(this.gameObject);
+        currentScene = Intro;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (currentScene == Intro) {
+
+            if (Input.GetKey(KeyCode.Space)) {
+
+                LoadGame();
+            }
+        }
+
+        if (currentScene == SampleScene) {
+
+            if (Input.GetKey(KeyCode.Space)) {
+
+                LoadMemory();
+            }
+        }
+
     }
+
+    //To load a scene:
+
+    private void LoadScene(string scene)
+    {
+
+        SceneManager.LoadScene(scene);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+        currentScene = scene.name;
+    }
+
+    //Classic scenes loaders
+
+    public void LoadIntro()
+    {
+
+        LoadScene(Intro);
+    }
+
+    public void LoadGame()
+    {
+
+        LoadScene(SampleScene);
+    }
+
+    public void LoadEnding()
+    {
+
+        LoadScene(Ending);
+    }
+
+    //Mini games scenes loaders
+
+    public void LoadMemory()
+    {
+
+        LoadScene(Memory);
+    }   
 }
