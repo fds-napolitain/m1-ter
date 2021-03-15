a327410d1fb0589c65b0e2350ceb55389153ef9e
Maxime Boucher <mxboucher@gmail.com>
2021-03-15 18:43:00 +0100

    advancing memory

--- Assets/Scripts/Memory.cs
+++ Assets/Scripts/Memory.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Memory : MonoBehaviour
 {
+    private enum CardID
+    {
+        SUN,
+        ASTEROID,
+        BANANA,
+        SATELLITE,
+        MOON,
+        ALIENSHIP,
+    }
 
-    Sprite carte;
-    Sprite carte_dos;
-
-    public static string carte_prec;
-
-    private int cp = 0;
-
-    private int nbCarte = 0;
-
-    public string carte_Name;
-
-    private int erreur = 2; //maximum 2
-
-    private bool isUpsideDown = false;
-
+    // game variables
+    private static string carte_prec;
+    private static int errors = 0;
+    private static int nbCarte = 0;
+    private static int ERRORS_MAX = 2; //maximum 2
+    private static List<CardID> spritesName = new List<CardID>(); 
+    // card variables
+    private Sprite carte;
+    private Sprite carte_dos;
     private SpriteRenderer spriteRenderer;
+    private bool isFacingCard = false;
 
 
     // Start is called before the first frame update
@@ -28,61 +34,58 @@ public class Memory : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         carte_dos = spriteRenderer.sprite;
-
+        spritesName.Add(CardID.SUN);
+        spritesName.Add(CardID.SUN);
+        spritesName.Add(CardID.ASTEROID);
+        spritesName.Add(CardID.ASTEROID);
+        spritesName.Add(CardID.BANANA);
+        spritesName.Add(CardID.BANANA);
+        spritesName.Add(CardID.SATELLITE);
+        spritesName.Add(CardID.SATELLITE);
+        spritesName.Add(CardID.MOON);
+        spritesName.Add(CardID.MOON);
+        spritesName.Add(CardID.ALIENSHIP);
+        spritesName.Add(CardID.ALIENSHIP);
+        spritesName = spritesName.OrderBy(card => Guid.NewGuid()).ToList(); // aleatoire
+        Debug.Log(this.name);
     }
-
-    public string Carte_Name
-    {
-        get
-        {
-            return carte_Name;
-        }
-        set
-        {
-            carte_Name = value;
-        }
-    }
-
 
     private void OnMouseDown() //This function is called each time player clicks on GameObject
     {
-        if (!isUpsideDown && cp < erreur)
-        {
-            ChangeSide();
-            nbCarte++;
-            if(nbCarte == 2)
-            {
-                if(carte.Carte_Name == carte_prec)
-                {
-                    cp++;
-                    //retourner les cartes
-                }
-                nbCarte = 0;
+        if (!isFacingCard) {
+            switch (name) {
+                case "card1":
+                    spriteRenderer.sprite = sprites
+                    break;
+                case "card2":
+                    break;
+                case "card3":
+                    break;
+                case "card4":
+                    break;
+                case "card5":
+                    break;
+                case "card6":
+                    break;
+                case "card7":
+                    break;
+                case "card8":
+                    break;
+                case "card9":
+                    break;
+                case "card10":
+                    break;
+                case "card11":
+                    break;
+                case "card12":
+                    break;
             }
-            if(nbCarte == 1)
-            {
-                carte_prec = carte.Carte_Name;
-            }
+        } else {
+
         }
     }
 
     void ChangeSide()
-    {
-        if (!isUpsideDown)
-        {
-            spriteRenderer.sprite = carte;
-            isUpsideDown = true;
-        }
-        else
-        {
-            spriteRenderer.sprite =carte_dos;
-            isUpsideDown = false;
-        }
-    }
-
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
