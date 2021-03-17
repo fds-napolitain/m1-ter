8547cbb6bf7f9d5e12a5b30a3bf348e4581437c4
MCeresole <merelo@live.fr>
2021-03-17 12:08:29 +0100

    début simon

--- Assets/Scripts/Simon.cs
+++ Assets/Scripts/Simon.cs
@@ -4,40 +4,74 @@ using UnityEngine;
 
 public class Simon : MonoBehaviour
 {
-    List<Sprite> list_sprite;
 
-    List<int> list_id_sprite;
+    static List<int> ordre = new List<int>();
 
-    int cp = 0;
+    static List<Sprite> list_sprite = new List<Sprite>();
+    static List<string> ordreBouton = new List<string>();
 
+    private static int cp = 0;
+    private static int nbreCouleur = 4;
+    private static bool flag = false;
     private SpriteRenderer spriteRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //spriteRenderer = GetComponent<SpriteRenderer>();
-        //action qui fait le simon mdr
-        //initialise list_id_sprite
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (!flag)
+        {
+            Random aleatoire = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                ordre.Add(aleatoire.Next(1, 10));
+                ordreBouton.Add("bouton" + ordre[ordre.Count -1]);
+            }
+            flag = true;
+            AfficheOrdre(nbreCouleur);
+        }
     }
 
-    /*private void OnMouseDown() //This function is called each time player clicks on GameObject
-    {
-        if(list_sprite.FindIndex(spriteRenderer.sprite) == list_id_sprite.Find(cp))
-        {
-            if(cp == 4) //finish
-            cp++;
-            //action qui fait que la carte s'allume
-        }
-        else
-        {
-            cp = 0;
-        }
-    }*/
+    
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
+
+    void AfficheOrdre(int n) { 
+        //dans une boucle remplacer les sprites correspondant par un sprite couleur puis les remettre noirs
+    }
+
+
+    private void OnMouseDown() //This function is called each time player clicks on GameObject
+    {
+        if (ordreBouton[cp] == name)
+        {
+            yield return new WaitForSeconds(1);
+            spriteRenderer.sprite = couleur;
+            cp++;
+            yield return null;
+            if (cp == nbreCouleur)
+            {
+                if(nbreCouleur == 6)
+                {
+                    SceneManager.LoadScene("Corridor_AA");
+                }
+                nbreCouleur++;
+                cp = 0;
+                AfficheOrdre(nbreCouleur);
+            }
+            spriteRenderer.sprite = black;
+        }
+
+    }
+    yield return new WaitForSeconds(1);
+    spriteRenderer.sprite = error;
+    yield return null;
+    SceneManager.LoadScene("Hangar_AB");
 }
