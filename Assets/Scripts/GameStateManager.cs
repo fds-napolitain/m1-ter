42d67bc8d22c900b065748fbc0279a71a8608011
Pauline C <47113664+NeonVhenan@users.noreply.github.com>
2021-05-10 15:27:03 +0200

    Added escape as exit button

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -79,6 +79,11 @@ public class GameStateManager : MonoBehaviour
             }
         }
 
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
         /*if (currentScene == game) {
 
             if (Input.GetKey(KeyCode.Space)) {
